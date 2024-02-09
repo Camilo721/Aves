8b3a7f3414929c0a19ad4e9bce4c52d490a9fcc0
camilo <camilo@DESKTOP-3QA99QR.mshome.net>
2024-02-09 12:51:36 -0500

    project deep to the repository

---
+++ src/main/java/com/mycompany/aves/Ave.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aves;
+
+/**
+ *
+ * @author camilo
+ */
+public abstract class Ave {
+    
+    public abstract void makeSound();
+}
